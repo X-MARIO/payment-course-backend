a00a61f0a6c32e4cfaee810c13c6c8e1f8c7d3c8
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-09-09 21:59:51 +0400

    add cors config

---
+++ src/config/cors.config.ts
@@ -0,0 +1,11 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
+import { ConfigService } from '@nestjs/config'
+
+export const getCorsConfig: (configService: ConfigService) => CorsOptions = (
+	configService: ConfigService
+) => {
+	return {
+		origin: configService.getOrThrow<string>('HTTP_CORS').split(','),
+		credentials: true
+	}
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,30 @@
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import * as process from 'node:process'
 
 import { AppModule } from './app.module'
+import { getCorsConfig } from './config'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
-	await app.listen(process.env.PORT ?? 3000)
+
+	const config = app.get(ConfigService)
+	const logger = new Logger(AppModule.name)
+
+	app.enableCors(getCorsConfig(config))
+
+	const port = config.getOrThrow<number>('HTTP_PORT')
+	const host = config.getOrThrow<number>('HTTP_HOST')
+
+	try {
+		await app.listen(port)
+
+		logger.log(`Server is running at: ${host}`)
+	} catch (error) {
+		logger.error(`Failed to start server: ${error.message}`, error)
+		process.exit(1)
+	}
 }
+
 bootstrap()
