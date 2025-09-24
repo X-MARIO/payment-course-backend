f71ade188aef3984998efd15ec072fa14be5d31c
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-09-24 14:52:04 +0400

    update test e2e

--- test/e2e/users/users.e2e-spec.ts
+++ test/e2e/users/users.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { INestApplication } from '@nestjs/common';
+import * as http from 'node:http';
 import * as request from 'supertest';
 
 import { createTestApp } from '../utils';
@@ -6,20 +7,18 @@ import { createTestApp } from '../utils';
 describe('UsersController (e2e)', () => {
   let app: INestApplication;
 
-  // Перед каждым тестом создаем приложение с помощью нашей утилиты
   beforeEach(async () => {
     app = await createTestApp();
   });
 
-  // После каждого теста корректно закрываем приложение
   afterEach(async () => {
     await app.close();
   });
 
   it('GET /api/v1/users -> should get an empty array of users', () => {
-    return request(app.getHttpServer())
+    return request(app.getHttpServer() as http.Server)
       .get('/api/v1/users')
       .expect(200)
-      .expect([]); // Ожидаем пустой массив
+      .expect([]);
   });
 });
