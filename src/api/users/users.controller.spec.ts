c45caaa1536fd92c829e35ec2fda73470e8d46dd
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-09-24 14:56:07 +0400

    update unit test users

--- src/api/users/users.controller.spec.ts
+++ src/api/users/users.controller.spec.ts
@@ -1,13 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
 
+  // Создаем фейковый объект UsersService
+  const mockUsersService: UsersService = {
+    // Здесь можно имитировать методы, которые вызывает контроллер.
+    // Например, если бы был метод findAll:
+    findAll: jest.fn(() => Promise.resolve([])),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          // 2. Указываем, что для токена UsersService нужно использовать наш мок
+          provide: UsersService,
+          useValue: mockUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
