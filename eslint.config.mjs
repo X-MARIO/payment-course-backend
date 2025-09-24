b2bda24a9bbbd36d55996c5c0a203545b029aff6
Pavel Chekushkin <x-chekushkin-pavel@yandex.ru>
2025-09-24 12:42:49 +0400

    update ci.yml

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 // @ts-check
 import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+// Импортируем плагин безопасности
+import security from 'eslint-plugin-security';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -11,6 +13,8 @@ export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  // Включаем рекомендованный набор правил безопасности
+  security.configs.recommended,
   {
     languageOptions: {
       globals: {
@@ -28,7 +32,7 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
     },
   },
 );
